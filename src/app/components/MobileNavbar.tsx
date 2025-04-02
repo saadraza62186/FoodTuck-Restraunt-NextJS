2ad40a3c7005e538d6e131d91ba67d64af87351d
Saad Raza <razasaad62186gmail.com>
2025-04-02 20:07:36 +0500

    added link

--- src/app/components/MobileNavbar.tsx
+++ src/app/components/MobileNavbar.tsx
@@ -1,56 +1,78 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import MenuIcon from '@mui/icons-material/Menu';
+import { Menu } from "lucide-react";
+import Image from "next/image";
 
 const MobileNavbar = () => {
   return (
     <div>
       <Sheet>
         {/* Trigger Icon */}
-        <SheetTrigger>
-          <MenuIcon className="text-white cursor-pointer" />
+        <SheetTrigger className="flex items-center justify-center">
+          <Menu className="h-6 w-6 text-white cursor-pointer" />
         </SheetTrigger>
 
         {/* Drawer Content */}
-        <SheetContent className="bg-white text-black">
-          <SheetHeader>
-            <SheetDescription>
-              {/* Menu Links */}
-              <div className="flex flex-col text-black gap-4 text-base font-bold mt-6">
-                <Link href="/">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Home</div>
-                </Link>
-                <Link href="/menu">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Menu</div>
-                </Link>
-                <Link href="/blog">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Blog</div>
-                </Link>
-                <Link href="/pages">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Pages</div>
-                </Link>
-                <Link href="/about">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">About</div>
-                </Link>
-                <Link href="/shop">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Shop</div>
-                </Link>
-                <Link href="/contact">
-                  <div className="cursor-pointer hover:text-yellow-500 border-b-2 pb-3">Contact</div>
-                </Link>
-              </div>
-            </SheetDescription>
+        <SheetContent side="left" className="bg-white w-[300px]">
+          <SheetHeader className="mb-4">
+            <div className="h-14 flex items-center bg-black justify-center rounded-tl-lg rounded-tr-lg">
+              {/* You can add your logo here */}
+              <Image
+                src="/Foodtuck.png"
+                alt="Foodtuck"
+                width={109}
+                height={32}
+                quality={100}
+                placeholder="empty"
+                className="w-auto h-auto 2xl:w-full 2xl:h-[30px]"
+              />
+            </div>
           </SheetHeader>
+
+          {/* Menu Links */}
+          <div className="flex flex-col text-black gap-4 text-base font-bold mt-6 ">
+            <Link href="/" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Home
+              </div>
+            </Link>
+            <Link href="/menu" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Menu
+              </div>
+            </Link>
+            <Link href="/blog" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Blog
+              </div>
+            </Link>
+            <Link href="/pages" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Pages
+              </div>
+            </Link>
+            <Link href="/about" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                About
+              </div>
+            </Link>
+            <Link href="/shop" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Shop
+              </div>
+            </Link>
+            <Link href="/contact" className="w-full">
+              <div className="cursor-pointer hover:text-yellow-500 border-b border-gray-200 pb-3 text-black">
+                Contact
+              </div>
+            </Link>
+          </div>
         </SheetContent>
       </Sheet>
     </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,69 +1,69 @@
-
 // Navbar Component
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import Link from 'next/link';
-import MobileNavbar from './MobileNavbar';
+import React, { useState } from "react";
+import Image from "next/image";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import Link from "next/link";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className='bg-transparent w-full h-[90px] flex justify-between items-center px-4 sm:px-6 md:px-10 lg:px-12 2xl:w-[1320px] 2xl:px-0'>
+    <div className="bg-transparent w-full h-[90px] flex justify-between items-center px-4 sm:px-6 md:px-10 lg:px-12 2xl:w-[1320px] 2xl:px-0">
       {/* Logo */}
-      <div className='flex-shrink-0 2xl:w-[109px] 2xl:h-[32px]'>
-  <Image
-    src='/Foodtuck.png'
-    alt='Foodtuck'
-    width={109}
-    height={32}
-    quality={100}
-    placeholder='empty'
-    className='w-auto h-auto 2xl:w-full 2xl:h-[30px]'
-  />
-</div>
-
+      <div className="flex-shrink-0 2xl:w-[109px] 2xl:h-[32px]">
+        <Image
+          src="/Foodtuck.png"
+          alt="Foodtuck"
+          width={109}
+          height={32}
+          quality={100}
+          placeholder="empty"
+          className="w-auto h-auto 2xl:w-full 2xl:h-[30px]"
+        />
+      </div>
 
       {/* Navigation Links */}
-      <div className={`hidden md:flex text-white gap-4 lg:gap-6 text-sm lg:text-base 2xl:gap-10 2xl:font-bold 2xl:text-[16px]`}>
-        <Link href='/'>
-        <h2 className="cursor-pointer hover:text-yellow-500">Home</h2>
+      <div
+        className={`hidden md:flex text-white gap-4 lg:gap-6 text-sm lg:text-base 2xl:gap-10 2xl:font-bold 2xl:text-[16px]`}
+      >
+        <Link href="/">
+          <h2 className="cursor-pointer hover:text-yellow-500">Home</h2>
         </Link>
         <Link href="/menu">
-          <h2 className='cursor-pointer hover:text-yellow-500'>Menu</h2>
+          <h2 className="cursor-pointer hover:text-yellow-500">Menu</h2>
         </Link>
-        <Link href='/blog'>
-        <h2 className='cursor-pointer hover:text-yellow-500'>Blog</h2>
+        <Link href="/blog">
+          <h2 className="cursor-pointer hover:text-yellow-500">Blog</h2>
         </Link>
-        <Link href='/chef'>
-        <h2 className='cursor-pointer hover:text-yellow-500'>Chef</h2>
+        <Link href="/chef">
+          <h2 className="cursor-pointer hover:text-yellow-500">Chef</h2>
         </Link>
-        <Link href='/about'>
-        <h2 className='cursor-pointer hover:text-yellow-500'>About</h2>
+        <Link href="/about">
+          <h2 className="cursor-pointer hover:text-yellow-500">About</h2>
         </Link>
-        <Link href='/shop'>
-        <h2 className='cursor-pointer hover:text-yellow-500'>Shop</h2>
+        <Link href="/shop">
+          <h2 className="cursor-pointer hover:text-yellow-500">Shop</h2>
         </Link>
-        <Link href='/faq'>
-        <h2 className='cursor-pointer hover:text-yellow-500'>Faq</h2>
+        <Link href="/faq">
+          <h2 className="cursor-pointer hover:text-yellow-500">Faq</h2>
         </Link>
       </div>
 
       {/* Icons */}
-      <div className='text-white flex gap-3 sm:gap-4 lg:gap-5 2xl:gap-6'>
-        <SearchIcon className='cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]' />
-        <PersonIcon className='cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]' />
-        <ShoppingBagIcon className='cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]' />
+      <div className="text-white flex gap-3 sm:gap-4 lg:gap-5 2xl:gap-6">
+        <SearchIcon className="cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]" />
+        <PersonIcon className="cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]" />
+        <ShoppingBagIcon className="cursor-pointer hover:text-yellow-500 w-5 h-5 lg:w-6 lg:h-6 2xl:w-[24px] 2xl:h-[24px]" />
       </div>
 
       {/* Mobile Menu */}
-      <div className='md:hidden flex items-center'>
-        <div className='text-white'>
+      <div className="md:hidden flex items-center">
+        <div className="text-white">
           <MobileNavbar />
         </div>
       </div>
